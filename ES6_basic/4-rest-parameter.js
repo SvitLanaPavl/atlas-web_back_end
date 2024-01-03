7619cb4096cf5d94c9e851b30e0edef795f735c7
Pavlovska Svitlana <lanapavlovska90@gmail.com>
2024-01-03 21:57:56 +0000

    eslint unused variables muted

--- ES6_basic/4-rest-parameter.js
+++ ES6_basic/4-rest-parameter.js
@@ -1,6 +1,8 @@
 export default function returnHowManyArguments(...args) {
   let total = 0;
   for (const arg of args) {
+    // eslint-disable-next-line no-unused-vars
+    const _ = arg;
     total += 1;
   }
   return total;
