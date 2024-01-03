31c0f35e71ce61bfb3738daa87515815bfe1eb8e
Pavlovska Svitlana <lanapavlovska90@gmail.com>
2024-01-03 23:14:06 +0000

    const instead of let

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArr = [];
-  for (let value of array) {
+  for (const value of array) {
     newArr.push(appendString + value);
   }
 
