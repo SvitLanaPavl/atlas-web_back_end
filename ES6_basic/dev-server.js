db8957c31849798d0b6ae0286ae250f5698b162a
Pavlovska Svitlana <lanapavlovska90@gmail.com>
2024-01-03 23:12:46 +0000

    for of loop

--- ES6_basic/dev-server.js
+++ ES6_basic/dev-server.js
@@ -1,9 +1,2 @@
-import './1-main.js';
-import './2-main.js';
-import './3-main.js';
-import './4-main.js';
-import './5-main.js';
-import './6-main.js';
-import './7-main.js';
-import './8-main.js';
-import './9-main.js';
+
+import './10-main.js';
