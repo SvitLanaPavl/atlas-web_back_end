548f2c722982ee97b1a29b7cd81135421ace6bb5
Pavlovska Svitlana <lanapavlovska90@gmail.com>
2024-01-04 17:12:33 -0600

    some eslint

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -25,18 +25,21 @@ export default class HolbertonCourse {
   get students() {
     return this._students;
   }
+
   set name(newName) {
     if (typeof newName !== 'string') {
       throw new TypeError('Name must be a string');
     }
     this._name = newName;
   }
+
   set length(newLength) {
     if (typeof newLength !== 'number') {
       throw new TypeError('Length must be a number');
     }
     this._length = newLength;
   }
+
   set students(newStudents) {
     if (!Array.isArray(newStudents) || !newStudents.every(student => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
