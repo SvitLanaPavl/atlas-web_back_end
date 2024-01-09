5747818264f5be1e81fa5727912f0a404539858a
Pavlovska Svitlana <lanapavlovska90@gmail.com>
2024-01-09 16:55:04 -0600

    needs dataview object

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
-  const arrbuffer = new ArrayBuffer(length);
-  const int8 = new Int8Array(arrbuffer);
-  int8[position] = value;
   if (position < 0 || position >= length) {
     throw new Error('Position outside range');
   }
-  return arrbuffer;
+  const arrbuffer = new ArrayBuffer(length);
+  const int8 = new Int8Array(arrbuffer);
+  int8[position] = value;
+  return new DataView(arrbuffer);
 }
