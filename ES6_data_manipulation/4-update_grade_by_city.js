40152bdd176c234ee1e937f77fea609b24efb267
Pavlovska Svitlana <lanapavlovska90@gmail.com>
2024-01-09 16:25:26 -0600

    there was the wrong key name in grade object and the eslint resolved

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,12 @@
 export default function updateStudentGradeByCity(list, city, newGrades) {
   const filtStu = list.filter((item) => item.location === city);
   const mapStu = filtStu.map((item) => {
-    const newGrade = newGrades.find((grade) => grade.stuId === item.id);
+    const newGrade = newGrades.find((grade) => grade.studentId === item.id);
     const grade = newGrade ? newGrade.grade : 'N/A';
     return {
       ...item,
       grade,
     };
-    });
+  });
   return mapStu;
 }
