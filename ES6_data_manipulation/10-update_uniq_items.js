1583ef4c3f5915549981b02186038a432408eac2
Pavlovska Svitlana <lanapavlovska90@gmail.com>
2024-01-09 23:27:14 -0600

    now should update 100 correctly

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -2,7 +2,7 @@ export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   }
-  for (const [value, key] of map) {
+  for (const [key, value] of map) {
     if (value === 1) {
       map.set(key, 100);
     }
