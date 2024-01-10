0e7d891630ee6e1e35b5d15fc286637dd5401470
Pavlovska Svitlana <lanapavlovska90@gmail.com>
2024-01-10 13:46:40 -0600

    it should be promise.resolve not new promise

--- ES6_promise/utils.js
+++ ES6_promise/utils.js
@@ -1,17 +1,13 @@
 export function uploadPhoto() {
-  return new Promise(() => {
-    return {
+  return Promise.resolve({
       status: 200,
       body: 'photo-profile-1',
-    }
-  })
+    });
 }
 
 export function createUser() {
-  return new Promise(() => {
-    return {
+  return Promise.resolve({
       firstName: 'Guillaume',
       lastName: 'Salva',
-    }
-  })
+    });
 }
