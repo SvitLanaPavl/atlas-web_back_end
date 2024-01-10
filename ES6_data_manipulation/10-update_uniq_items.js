e331ca6aa007522ede7d21a67c1741cea7a9ef84
Pavlovska Svitlana <lanapavlovska90@gmail.com>
2024-01-09 23:09:38 -0600

    refactored

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -2,9 +2,13 @@ export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   }
-  const newMap = new Map;
+  const newMap = new Map();
   for (const [key, value] of map) {
-    value === 1 ? newMap.set(key, 100) : newMap.set(key, value);
+    if (value === 1) {
+      newMap.set(key, 100);
+    } else {
+      newMap.set(key, value); 
     }
-    return newMap;
   }
+  return newMap;
+}
