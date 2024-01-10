722404f3930e36013429b4e2e8d692bf9f8a2d01
Pavlovska Svitlana <lanapavlovska90@gmail.com>
2024-01-10 13:52:46 -0600

    maybe instead console error i need console log

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -6,6 +6,6 @@ export default function handleProfileSignup() {
       console.log(`${uploadPhoto.body} ${createUser.firstName} ${createUser.lastName}`);
     })
     .catch(() => {
-      console.error('Signup system offline');
+      console.log('Signup system offline');
     });
 }
