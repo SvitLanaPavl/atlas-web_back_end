257a16a4edddf377b95f5968014819eab5897d7f
Pavlovska Svitlana <lanapavlovska90@gmail.com>
2024-01-10 14:23:59 -0600

    looks like foreach doesn't return a new array - so map

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,8 +4,8 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
   return Promise.allSettled(promises).then((results) =>
-  results.forEach((result) => ({
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
-  })));
+    results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
 }
