0b8ea9a2b29ad5ecb30cbfc6c2a57ea4966efd2b
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-04-29 11:07:00 -0500

    import typing List needed

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Module documentation'''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''Returns the sum as a float'''
+    return sum(input_list)
