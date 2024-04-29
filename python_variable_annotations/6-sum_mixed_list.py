0476177a5b83864edcf64c67c17782bd9ae47e05
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-04-29 11:15:13 -0500

    Union is needed for mixed types

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Module documentation'''
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    '''Mixed list returns the sum in float'''
+    return sum(mxd_lst)
