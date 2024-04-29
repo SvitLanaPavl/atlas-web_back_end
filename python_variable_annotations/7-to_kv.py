cb21c40b9a78156bcc1bf3aed90ab6d05539fb64
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-04-29 11:35:28 -0500

    | looks like unsupported operand

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,6 +3,6 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int | float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''Returns a tuple'''
-    return (k, float(v * v))
+    return (k, v * v)
