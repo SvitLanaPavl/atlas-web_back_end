b16a830ab4c7eb86963bf3c6f8a9be4d3444db5f
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-04-29 10:53:54 -0500

    alias needed to avoid recursion due ti the same name of the custom and library function

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,2 +1,8 @@
+#!/usr/bin/env python3
+'''Module documentation'''
+from math import floor as math_floor
 
 
+def floor(n: float) -> int:
+    '''Returns floor of the float'''
+    return math_floor(n)
