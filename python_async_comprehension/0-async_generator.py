8274fcd984a45f3a09c898f6505c498f49e4df21
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-04-30 12:10:18 -0500

    generator return type

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 '''Module Documentation'''
 import asyncio
 from random import uniform
-from typing import List
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     '''Yields a random number in asynchronous operation'''
     for _ in range(10):
         await asyncio.sleep(1)
