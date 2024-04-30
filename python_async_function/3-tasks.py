4417defa18ee8d903372558f97d6650dabd23c85
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-04-30 11:35:33 -0500

    Task in return type annotation

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 '''Module Documentation'''
 import asyncio
+from asyncio import Task
+import 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> int:
+def task_wait_random(max_delay: int) -> Task:
     '''returns Task'''
     return asyncio.create_task(wait_random(max_delay))
