48a769d9a22ae0f90325d5963a7d45ec8bb5dfea
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-04-30 12:05:06 -0500

    return value

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ from random import uniform
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float]:
     '''Yields a random number in asynchronous operation'''
     for _ in range(10):
         await asyncio.sleep(1)
