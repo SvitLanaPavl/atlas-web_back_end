26b470a7b6b781acf1b0ab9be1b2761cca98893e
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-04-30 12:04:19 -0500

    annotation type in return is async generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 '''Module Documentation'''
 import asyncio
 from random import uniform
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None, None]:
     '''Yields a random number in asynchronous operation'''
     for _ in range(10):
         await asyncio.sleep(1)
