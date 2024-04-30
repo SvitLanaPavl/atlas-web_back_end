5f07204e043a9a96a31031311c7f1fa7768e38d7
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-04-30 12:08:14 -0500

    removed return annotation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 '''Module Documentation'''
 import asyncio
 from random import uniform
-from typing import AsyncGenerator
+from typing import List
 
 
-async def async_generator() -> AsyncGenerator[float]:
+async def async_generator():
     '''Yields a random number in asynchronous operation'''
     for _ in range(10):
         await asyncio.sleep(1)
