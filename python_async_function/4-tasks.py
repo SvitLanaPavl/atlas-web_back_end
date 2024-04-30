b2832cb292b07ec1c103243dac24aef315b85b52
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-04-30 11:41:26 -0500

    pycodestyle length of line

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -7,9 +7,9 @@ task_wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     '''Returns list of delays in ascending order'''
-    delay = await asyncio.gather(*(task_wait_random(max_delay) for _ in range(n)))
-    for i in range(len(delay)):
-        for j in range(i + 1, len(delay)):
-            if delay[i] >= delay[j]:
-                delay[i], delay[j] = delay[j], delay[i]
-    return delay
+    dl = await asyncio.gather(*(task_wait_random(max_delay) for _ in range(n)))
+    for i in range(len(dl)):
+        for j in range(i + 1, len(dl)):
+            if dl[i] >= dl[j]:
+                dl[i], dl[j] = dl[j], dl[i]
+    return dl
