b3de5f298dd0cb923d648e4736010ba4a2183876
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-04-30 11:12:14 -0500

    int annotation in argument

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 '''Module Documentation'''
-wait_random = __import__('0-basic_async_syntax').wait_random
 from typing import List
 import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     '''Returns list of delays in ascending order'''
     delay = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
     for i in range(len(delay)):
