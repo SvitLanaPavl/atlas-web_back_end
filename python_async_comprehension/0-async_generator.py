01e62a8b0313e04c720e9fa6c562f89a40afe7c6
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-04-30 11:59:13 -0500

    task 0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''Module Documentation'''
+import asyncio
+from random import uniform
+
+
+async def async_generator():
+    '''Yields a random number in asynchronous operation'''
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield uniform(0, 10)
