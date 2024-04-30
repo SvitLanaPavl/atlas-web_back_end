41411a26bd4c56658814b50814d682277fb913fc
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-04-30 11:33:33 -0500

    task 3

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Module Documentation'''
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int):
+    '''returns Task'''
+    return asyncio.create_task(wait_random(max_delay))
