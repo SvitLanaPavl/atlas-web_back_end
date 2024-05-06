d1a8a849cd28e5484cf0307213b1d6740992bc05
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-05-06 14:25:11 -0500

    back to previous variant

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -11,23 +11,19 @@ class LRUCache(BaseCaching):
     def __init__(self):
         '''Constructor'''
         super().__init__()
-        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         '''Assigns key to dictionary'''
         if key is None or item is None:
             return
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            self.cache_data.popitem(last=False)
-            print(f'DISCARD: {key}')
-        if key in self.cache_data:
-            del self.cache_data[key]
+            discarded = list(self.cache_data.keys())[0]
+            del self.cache_data[discarded]
+            print(f'DISCARD: {discarded}')
         self.cache_data[key] = item
 
     def get(self, key):
         '''Returns the value'''
         if key is None or key not in self.cache_data:
             return None
-        value = self.cache_data.pop(key)
-        self.cache_data[key] = value
-        return value
+        return self.cache_data[key]
