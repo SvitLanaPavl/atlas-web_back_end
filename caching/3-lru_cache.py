a579ccb98208705d0e38e2303a4f768f8f14aab2
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-05-06 13:03:00 -0500

    task 3

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -1,1 +1,28 @@
 #!/usr/bin/env python3
+'''LRU caching'''
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    '''LRU caching system'''
+    def __init__(self):
+        '''Constructor'''
+        super().__init__()
+
+    def put(self, key, item):
+        '''Assigns key to dictionary'''
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            discarded = next(iter(self.cache_data))
+            del self.cache_data[discarded]
+            print(f'DISCARDED: {discarded}')
+        self.cache_data[key] = item
+
+    def get(self, key):
+        '''Returns the value'''
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
