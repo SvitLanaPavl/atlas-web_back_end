d653104b6eafd286d4f4dd949615fc1a63f519e8
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-05-06 12:38:07 -0500

    task 2

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -1,1 +1,28 @@
 #!/usr/bin/env python3
+'''LIFO caching'''
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    '''LIFO Caching system'''
+    def __init__(self):
+        '''Constructor'''
+        super().__init__()
+
+    def put(self, key, item):
+        '''Assigning value to dictionary'''
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            discarded = list(self.cache_data.keys())[-1]
+            del self.cache_data[discarded]
+            print("DISCARD:", discarded)
+        self.cache_data[key] = item
+
+    def get(self, key):
+        '''Returns the value of key'''
+        if key is not None or key in self.cache_data:
+            return self.cache_data[key]
+        return None
