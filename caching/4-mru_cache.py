b43eb8b13a8ad1c467f4abf8fdeae7a912061f95
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-05-07 12:18:01 -0500

    ordered dict added

--- caching/4-mru_cache.py
+++ caching/4-mru_cache.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 '''MRU caching'''
+from collections import OrderedDict
 
 
 BaseCaching = __import__('base_caching').BaseCaching
@@ -10,19 +11,22 @@ class MRUCache(BaseCaching):
     def __init__(self):
         '''Constructor'''
         super().__init__()
+        self.order = OrderedDict()
 
     def put(self, key, item):
         '''Assign key to dict'''
         if key is None or item is None:
             return
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            discard = next(reversed(self.cache_data))
+            discard = next(reversed(self.order))
             del self.cache_data[discard]
             print(f'DISCARD: {discard}')
         self.cache_data[key] = item
+        self.order[key] = True
 
     def get(self, key):
         '''Returns the value'''
         if key is None or key not in self.cache_data:
             return None
+        self.order.move_to_end(key)
         return self.cache_data[key]
