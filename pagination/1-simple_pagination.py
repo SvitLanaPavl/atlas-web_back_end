f5dd75be9d07969cfe7630e7b03e4660318691b0
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-05-07 13:09:05 -0500

    pycodestyle

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -12,6 +12,7 @@ def index_range(page, page_size):
     end_index = start_index + page_size
     return (start_index, end_index)
 
+
 class Server:
     """Server class to paginate a database of popular baby names.
     """
