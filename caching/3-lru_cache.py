2424cc2c63199758ed6e5eaef917706a38fd8724
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-05-07 11:50:34 -0500

    used ordered dict and moved to end most recently used in get

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -11,19 +11,23 @@ class LRUCache(BaseCaching):
     def __init__(self):
         '''Constructor'''
         super().__init__()
+        self.order = OrderedDict()
 
     def put(self, key, item):
         '''Assigns key to dictionary'''
         if key is None or item is None:
             return
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            discarded = list(self.cache_data.keys())[0]
+            discarded = next(iter(self.order))
+            del self.order[discarded]
             del self.cache_data[discarded]
             print(f'DISCARD: {discarded}')
         self.cache_data[key] = item
+        self.order[key] = True
 
     def get(self, key):
         '''Returns the value'''
         if key is None or key not in self.cache_data:
             return None
+        self.order.move_to_end(key)
         return self.cache_data[key]
