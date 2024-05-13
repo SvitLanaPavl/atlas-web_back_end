dd70f4a5582a520ccd0f813f0f0644f6bf94f8dc
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-05-13 12:19:46 -0500

    task 1 - do not really know what to do with FORMAT

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 '''Filtered Logger'''
 import re
 from typing import List
+import logging
 
 
 def filter_datum(
@@ -14,3 +15,23 @@ def filter_datum(
     pattern = '|'.join(re.escape(field) for field in fields)
     return re.sub(f'({pattern})=(.*?){re.escape(separator)}',
                   f'\\1={redaction}{separator}', message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        '''Constructor'''
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+        
+
+    def format(self, record: logging.LogRecord) -> str:
+        '''Filter values in incoming logs'''
+        log_message = record.getMessage()
+        return filter_datum(self.fields, self.REDACTION, log_message, self.SEPARATOR)
