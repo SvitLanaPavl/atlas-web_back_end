2a1ec707931cdf87cb2b0d337066e4387c757240
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-05-13 12:25:43 -0500

    annot in param

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -25,10 +25,10 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
+    def __init__(self, fields: List[str]):
         '''Constructor'''
         super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields(list)
+        self.fields = fields
         
 
     def format(self, record: logging.LogRecord) -> str:
