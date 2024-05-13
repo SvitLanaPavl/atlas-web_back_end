95bc4258ad8c7d6f410edc805f19e2a1d4610d84
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-05-13 12:03:03 -0500

    Return value annotated

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,7 +4,7 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     '''Returns the log message obfuscated'''
     pattern = '|'.join(re.escape(field) for field in fields)
     return re.sub(f'({pattern})=(.*?){re.escape(separator)}',
