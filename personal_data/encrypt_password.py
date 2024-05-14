b58981364bba4c792e1254ef803a8746ef89ec09
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-05-14 16:45:47 -0500

    validation

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -8,3 +8,9 @@ def hash_password(password: str) -> bytes:
     bytes = password.encode('utf-8')
     salt = bcrypt.gensalt(rounds=15)
     return bcrypt.hashpw(bytes, salt)
+
+
+def is_valid(hashed_password: bool, password: str) -> bool:
+    '''Validates provided password matches hashed'''
+    pass_bytes = hash_password(password)
+    return bcrypt.checkpw(hashed_password, pass_bytes)
