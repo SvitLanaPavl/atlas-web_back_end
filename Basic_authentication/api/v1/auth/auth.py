e241010df07f36f2b9f72ba3c610a9543fa9ce84
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-05-15 12:53:42 -0500

    require auth conditions

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,20 @@ class Auth():
     '''API authentication management'''
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         '''Require authentication'''
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path in excluded_paths:
+            return False
+        if not path.endswith('/'):
+            path += '/'
+        if not path.endswith('/'):
+            path += '/'
+        for excl_path in excluded_paths:
+            if path in excluded_paths and excl_path.endswith('/'):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         '''Auth header'''
