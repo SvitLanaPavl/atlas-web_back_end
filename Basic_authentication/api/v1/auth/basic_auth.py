9b138b1588f1c7609d3b4fc341d2796683e94f8d
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-05-15 15:35:04 -0500

    basic auth

---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+'''Manage the API authentication'''
+from flask import request
+from typing import TypeVar, List
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    '''Basic Auth class'''
+    pass
