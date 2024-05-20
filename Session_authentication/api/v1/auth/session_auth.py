8dde668b189da858b6c0e6812dde903d12596bbd
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-05-20 14:50:41 -0500

    session authentication using uuid session id is created and returned

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -6,8 +6,19 @@ from api.v1.auth.auth import Auth
 import base64
 import binascii
 from models.user import User
+import uuid
 
 
 class SessionAuth(Auth):
     '''Session authentication'''
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        '''Generates session id using uuid'''
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
