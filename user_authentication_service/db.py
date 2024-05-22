cc5ee5d00ef9798ebe4443e4a4c8f4c6609a45ae
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-05-21 22:07:28 -0500

    looks fine but wird return - the whole object instead of just id

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from typing import TypeVar
 
 from user import Base, User
 
@@ -28,7 +29,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> User:
+    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
         '''Saves the user to database'''
         new_usr = User(email=email, hashed_password=hashed_password)
         self._session.add(new_usr)
