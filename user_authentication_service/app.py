2087ac8c0ec0637d37489bb59ff468d5698c3d17
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-05-22 17:14:33 -0500

    still not sure how to set this session id to a cookie correctly

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''Route module for the API'''
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -24,6 +24,18 @@ def users() -> str:
         return jsonify({"email": f"{email}", "message": "user created"})
     except ValueError as e:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login() -> str:
+    '''Log in'''
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not email or not password:
+        abort(401)
+    session_id = AUTH.create_session(email)
+    set_cookie('session_id', session_id)
+    return jsonify({"email": f"{email}", "message": "logged in"})
 
 
 if __name__ == "__main__":
