a1395b6a45281b320dc91456eb7190e4d39208ad
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-05-22 18:58:04 -0500

    get reset password still not sure if it updates in the user object

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -69,3 +69,12 @@ class Auth:
         '''Destroy session'''
         self._db.update_user(user_id=user_id, session_id=None)
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        '''Generate reset password token'''
+        try:
+            user = self._db.find_user_by(email=email)
+            user.reset_token = _generate_uuid()
+            return user.reset_token
+        except NoResultFound:
+            raise ValueError('No user found')
