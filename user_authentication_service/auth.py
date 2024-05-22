7c6b09ac78922612fcc2c25e0c0d1fb8a0affcd4
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-05-22 16:44:43 -0500

    pycodestyle

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -41,6 +41,7 @@ class Auth:
         except NoResultFound:
             return False
 
+
 def _generate_uuid() -> str:
-        '''Str representation of the new uuid'''
-        return str(uuid.uuid4())
+    '''Str representation of the new uuid'''
+    return str(uuid.uuid4())
