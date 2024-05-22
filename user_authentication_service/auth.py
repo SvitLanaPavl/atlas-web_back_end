6fb5a89665da91dc6fa1a4ef2d8cea577dd7a73d
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-05-21 23:21:30 -0500

    had to refactor to have try and except noresultfound - if else did not work

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 '''Module doc hash password'''
 import bcrypt
+from db import DB
+from user import Base, User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -8,3 +11,21 @@ def _hash_password(password: str) -> bytes:
     byte_repr = password.encode('utf-8')
     salt = bcrypt.gensalt(rounds=15)
     return bcrypt.hashpw(byte_repr, salt)
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        '''Register user'''
+        try:
+            user = self._db.find_user_by(email=email)
+            raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            passwd = _hash_password(password)
+            new_usr = self._db.add_user(email, passwd)
+            return new_usr
