bad762e4e4ae5bcaa68a7b80435caaaa81071fad
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-05-22 19:33:21 -0500

    update password

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -81,5 +81,20 @@ def reset_password() -> str:
         abort(403)
 
 
+@app.route('/reset_password', methods=['PUT'])
+def update_password() -> str:
+    '''Update password end-point'''
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    if not reset_token:
+        abort(403)
+    new_password = request.form.get('new_password')
+    try:
+        AUTH.update_password(reset_token, password)
+        return jsonify({"email": f"{email}", "message": "Password updated"}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
