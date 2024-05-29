0af41e2a2a57bd8df771db4d0bb7f3f44fe776de
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-05-28 22:16:52 -0500

    looks like the test func hass all required positional arguments - we'll see

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -54,6 +54,14 @@ class TestGithubOrgClient(unittest.TestCase):
             mocked.assert_called_once
             mock_get_json.assert_called_once
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license"),
+        ({"license": {"key": "other_license"}}, "my_license")
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        '''Test has license'''
+        self.assertEqual(GithubOrgClient.has_license(repo, license_key), expected)
+
 
 if __name__ == '__main__':
     unittest.main()
