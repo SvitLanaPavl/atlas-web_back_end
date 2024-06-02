aae3b9ce5314e646ef38b4bf4631dc17d73618d5
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-06-02 17:20:20 -0500

    task 1 - implementation of get int and get str - trieve data from redis and decode bytes to str and to int

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -3,6 +3,7 @@
 import redis
 import uuid
 from typing import Union
+from typing import Callable, Optional
 
 
 class Cache:
@@ -17,3 +18,20 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data) #stores data in redis using generated key
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        '''Recovering the original type'''
+        data = self._redis.get(key)
+        if key is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        '''Get str'''
+        return self.get(key, lambda k: k.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        '''Get int'''
+        return self.get(key, lambda k: int(k))
