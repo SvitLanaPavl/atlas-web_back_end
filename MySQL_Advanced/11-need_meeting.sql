263ee6784d7737ccae92eb97d6ff58f2c2063da8
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-06-10 16:10:27 -0500

    see what the checkers says, locally claims doesn't have the field

---
+++ MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,15 @@
+-- No table for a meeting
+-- Creates a view that lists all students that have a score under 80
+CREATE VIEW need_meeting AS
+SELECT
+    students.name
+FROM
+    students
+LEFT JOIN
+    scores ON students.id = scores.student_id
+LEFT JOIN
+    meetings ON students.id = meetings.student_id
+WHERE
+    scores.score < 80
+    AND (meetings.last_meeting IS NULL
+        OR meetings.last_meeting < NOW() - INTERVAL 1 MONTH);
