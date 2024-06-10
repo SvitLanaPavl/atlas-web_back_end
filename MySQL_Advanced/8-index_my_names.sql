7244b62821b3009cca5ed85c07b2b23ab9b198ce
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-06-10 15:46:03 -0500

    index created directrly without virtual column yet displays nothing in interactive mode :/

--- MySQL_Advanced/8-index_my_names.sql
+++ MySQL_Advanced/8-index_my_names.sql
@@ -1,5 +1,3 @@
 -- Optimize simple search
 -- Creates an index on the table and the first letter of name
-ALTER TABLE names
-ADD COLUMN first_letter CHAR(1) GENERATED ALWAYS AS (LEFT(name, 1)) STORED;
-CREATE INDEX idx_name_first ON names (first_letter);
+CREATE INDEX idx_name_first ON names (name(1));
