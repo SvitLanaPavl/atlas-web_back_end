d1e72512d6179aa3759ebfa050c139e2fc2dcf8f
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-06-10 14:00:22 -0500

    so to get the correct result i need to downgrade to 2020, while putting the current year 2024 to substitute null for the split year will cause the output not as expected

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- Old school band
 -- Lists all bands with Glam rock as their main style
 SELECT band_name,
-COALESCE(split, YEAR(CURRENT_DATE)) - formed AS lifespan
+COALESCE(split, 2020) - formed AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
