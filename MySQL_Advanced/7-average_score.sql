e3d4414d9802ea6a31c6cb04ee6379aaa6da0efc
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-06-10 15:26:41 -0500

    task 7 done and tested

---
+++ MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,19 @@
+-- Average score
+-- Creates a stored procedure that computes and store the average score for a student
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN p_user_id INT
+)
+BEGIN
+    DECLARE avg_score DECIMAL(10, 2);
+    SELECT AVG(score) INTO avg_score
+    FROM corrections
+    WHERE user_id = p_user_id;
+
+    UPDATE users
+    SET average_score = avg_score
+    WHERE id = p_user_id;
+END //
+
+DELIMITER ;
