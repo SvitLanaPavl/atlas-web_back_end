f7368b30a9b2a68311203594872b07882614d91c
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-06-10 15:51:29 -0500

    syntax change

--- MySQL_Advanced/9-index_name_score.sql
+++ MySQL_Advanced/9-index_name_score.sql
@@ -1,3 +1,3 @@
 -- Optimize search and score
 -- Creates an index on the table and the first letter of name and score
-CREATE INDEX idx_name_first_score ON names (LEFT(name, 1), score)
+CREATE INDEX idx_name_first_score ON names (name(1), score)
