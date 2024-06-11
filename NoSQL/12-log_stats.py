bd622bec9ba26346acb3479090d8aa45142e9a24
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-06-11 11:24:43 -0500

    so not count but count_documents, if name = main not function name did not unzip with command but just dragged the unzipped file into the folder

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -3,16 +3,16 @@
 from pymongo import MongoClient
 
 
-def log_stats():
+if __name__ == '__main__':
     '''Stats about Nginx logs stored in MongoDB'''
     client = MongoClient('mongodb://127.0.0.1:27017')
     db = client.logs
     collection = db.Nginx
-    total_logs = collection.count({})
+    total_logs = collection.count_documents({})
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
     print('Methods:')
     for method in methods:
-        count = collection.count({'method': method})
+        count = collection.count_documents({'method': method})
         print(f'method {method}: {count}')
-    status_check = collection.count({'method': 'GET', 'path': '/status'})
+    status_check = collection.count_documents({'method': 'GET', 'path': '/status'})
     print(f'{status_check} status check')
