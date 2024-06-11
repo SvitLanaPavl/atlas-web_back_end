74e2933b15b50716041eda4e8bb3a532a60111cf
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-06-10 22:36:19 -0500

    surprisingly nothing happens although i tried several variants of getting the list of docs - looks like the db is empty - see what checkers says

---
+++ NoSQL/8-all.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+def list_all(mongo_collection):
+    '''Lists all documents in a collection'''
+    documents = []
+    for doc in mongo_collection.find():
+        documents.append(doc)
+    return documents
