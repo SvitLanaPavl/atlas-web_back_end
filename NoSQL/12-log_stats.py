1ed9a37e766b588dd719df33f466b1b604c20c96
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-06-11 11:30:13 -0500

    looks like it needed formatting with t to do tab

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -13,6 +13,6 @@ if __name__ == '__main__':
     print('Methods:')
     for method in methods:
         count = collection.count_documents({'method': method})
-        print(f'method {method}: {count}')
+        print(f'\tmethod {method}: {count}')
     status_check = collection.count_documents({'method': 'GET', 'path': '/status'})
     print(f'{status_check} status check')
