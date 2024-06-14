0885a9ae4617b6a27e51cec8d6524968b2e05de9
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-06-14 13:25:51 -0500

    not yet checked against eslint but the code works

---
+++ Node_JS/2-read_file.js
@@ -0,0 +1,27 @@
+const fs = require('node:fs');
+
+function countStudents(path) {
+    try {
+        const data = fs.readFileSync(path, 'utf8');
+        const lines = data.trim().split('\n');
+        const total = lines.shift().length;
+        fields = {};
+        lines.forEach(line => {
+            const [firstname, lastname, age, field] = line.split(',');
+            if (!fields[field]) {
+                fields[field] = [];
+            }
+            fields[field].push(firstname)
+        });
+        console.log(`Number of students: ${total}`);
+        for (const [field, students] of Object.entries(fields)) {
+            console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`)
+        }
+    } catch (err) {
+        console.error('Cannot load the database');
+        throw err;
+    }
+    
+}
+
+module.exports = countStudents;
