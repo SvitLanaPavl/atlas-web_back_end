0d20aa9302d8df6b8c476ca55641ade134dd173b
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-06-18 17:35:56 -0500

    added a couple more tests

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -15,4 +15,11 @@ describe('calculateNumber', function() {
   it('should return 6 when inputs are 1.5 and 3.7', function() {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
+  it('should return 0 when inputs are -1 and 1', function() {
+    assert.strictEqual(calculateNumber(-1, 1), 0);
+  });
+  it('should return -2 when inputs are -1 and -1', function() {
+    assert.strictEqual(calculateNumber(-1, -1), -2);
+  });
+  
 });
