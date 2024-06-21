86327325b6f985ff49b246dad0c248e39846fcb7
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-06-21 14:13:55 -0500

    added more tests from the test file of the checker

--- unittests_in_js/3-payment.test.js
+++ unittests_in_js/3-payment.test.js
@@ -7,12 +7,48 @@ describe('sendPaymentRequestAPI', function() {
     sinon.restore();
   })
 
-	it('calls the CalculateNumber with correct args', () => {
+	it('calls the CalculateNumber with correct args SUM whole', () => {
 		const calculateNumberSpy = sinon.spy(Utils, 'CalculateNumber');
 
 		sendPaymentRequestToApi(100, 20);
 		sinon.assert.calledOnce(calculateNumberSpy);
 		sinon.assert.calledOnceWithExactly(calculateNumberSpy, 'SUM', 100, 20);
+	});
+
+	it('calls the CalculateNumber with correct args SUM float', () => {
+		const calculateNumberSpy = sinon.spy(Utils, 'CalculateNumber');
+
+		sendPaymentRequestToApi(100.2, 20);
+		sinon.assert.calledOnce(calculateNumberSpy);
+		sinon.assert.calledOnceWithExactly(calculateNumberSpy, 'SUM', 100.2, 20);
+	});
+	it('calls the CalculateNumber with correct args SUM float and 0', () => {
+		const calculateNumberSpy = sinon.spy(Utils, 'CalculateNumber');
+
+		sendPaymentRequestToApi(1.3, 0);
+		sinon.assert.calledOnce(calculateNumberSpy);
+		sinon.assert.calledOnceWithExactly(calculateNumberSpy, 'SUM', 1.3, 0);
+	});
+	it('calls the CalculateNumber with correct args SUM float over .5 and 0', () => {
+		const calculateNumberSpy = sinon.spy(Utils, 'CalculateNumber');
+
+		sendPaymentRequestToApi(1.7, 0);
+		sinon.assert.calledOnce(calculateNumberSpy);
+		sinon.assert.calledOnceWithExactly(calculateNumberSpy, 'SUM', 1.7, 0);
+	});
+	it('calls the CalculateNumber with correct args SUM floats over .5', () => {
+		const calculateNumberSpy = sinon.spy(Utils, 'CalculateNumber');
+
+		sendPaymentRequestToApi(1.6, 1.8);
+		sinon.assert.calledOnce(calculateNumberSpy);
+		sinon.assert.calledOnceWithExactly(calculateNumberSpy, 'SUM', 1.6, 1.8);
+	});
+	it('calls the CalculateNumber with correct args SUM floats over .5 and under .5', () => {
+		const calculateNumberSpy = sinon.spy(Utils, 'CalculateNumber');
+
+		sendPaymentRequestToApi(1.7, 1.2);
+		sinon.assert.calledOnce(calculateNumberSpy);
+		sinon.assert.calledOnceWithExactly(calculateNumberSpy, 'SUM', 1.7, 1.2);
 	});
 
 	it('Displays the console message', () => {
