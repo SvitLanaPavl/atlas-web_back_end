f4381ea9aafbed40d865606923f2f1d231334a44
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-06-24 14:02:10 -0500

    publisher and subscriber: issue was in message display on server side, the calback args in the right order: channel, essage - no need to specify channel name again - it was kept as atg

--- queuing_system_in_js/4-redis_advanced_op.js
+++ queuing_system_in_js/4-redis_advanced_op.js
@@ -6,8 +6,8 @@ client.on('connect', () => {
   console.log('Redis client connected to the server');
 });
 
-client.on('error', () => {
-  console.log(`Redis client not connected to the server: ${err.message}`);
+client.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error.message}`);
 });
 
 // Hash
